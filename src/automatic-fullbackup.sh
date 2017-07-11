8af2af4695b8539c06ec43746f2b92defcd898ac
Dima73 <Dima-73@inbox.lv>
2017-07-11 07:42:07 +0300

    Fix MB Twin+

--- src/automatic-fullbackup.sh
+++ src/automatic-fullbackup.sh
@@ -185,7 +185,7 @@ if [ -f /proc/stb/info/boxtype ] ; then
 		MAINDEST="$DIRECTORY/miraclebox/microv2"
 		EXTRA="$DIRECTORY/automatic_fullbackup/$DATE"
 		echo "Destination        = $MAINDEST\n"
-	elif grep g300 /proc/stb/info/boxtype > /dev/null ; then
+	elif grep mbtwinplus /proc/stb/info/boxtype > /dev/null ; then
 		TYPE=MIRACLEBOX
 		MKUBIFS_ARGS="-m 2048 -e 126976 -c 8192"
 		UBINIZE_ARGS="-m 2048 -p 128KiB"
